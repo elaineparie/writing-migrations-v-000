0da5e843f4e48bc34d7632a07915b5b8add63eb1
Elaine Anderson <eparie@bu.edu>
2018-07-04 02:30:26 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
